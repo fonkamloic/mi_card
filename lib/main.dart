de24d94e76998814a48694f713237f9ab144a3a5
fonkamloic <fonkamloic@gmail.com>
2019-10-20 23:20:21 +0200

    Adding custom fonts

--- lib/main.dart
+++ lib/main.dart
@@ -12,36 +12,62 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.blue,
-        body: SafeArea(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Container(
-                height: 100.0,
-                width: 100.0,
-                color: Colors.white,
-                child: Text('Hello'),
-              ),
-
-              //spacing
-              SizedBox(
-                height: 20,
-              ),
-
-              Container(
-                width: 100.0,
-                height: 100.0,
-                color: Colors.blue,
-                child: Text('Container 2'),
-              ),
-              Container(
-                width: 100.0,
-                height: 100.0,
-                color: Colors.red,
-                child: Text('Container 3'),
-              ),
-            ],
+        backgroundColor: Colors.teal,
+        body: Center(
+          child: SafeArea(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 60.0,
+                  backgroundImage: AssetImage(
+                    "assets/images/loic.jpg",
+                  ),
+                ),
+                Row(
+                  children: <Widget>[
+                    Text(
+                      'Fonkam Loic',
+                      style: TextStyle(
+                        fontFamily: 'Zhi',
+                        fontSize: 40.0,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    Text(
+                      'FLUTTER DEVELOPER',
+                      style: TextStyle(
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 24.0,
+                        color: Colors.grey,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+                TextFormField(
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Please enter some text';
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Please enter some text';
+                    }
+                    return null;
+                  },
+                )
+              ],
+            ),
           ),
         ),
       ),
