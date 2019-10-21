3e1ff5ec80b0cecc3d15a97551970536f8aa54b9
fonkamloic <fonkamloic@gmail.com>
2019-10-21 03:05:48 +0200

    Adding dice random game

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:ui' as prefix0;
+
 import 'package:flutter/cupertino.dart';
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -107,19 +110,61 @@ class Mi_card extends StatelessWidget {
   }
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = new Random().nextInt(6);
+  int rightDiceNumber = new Random().nextInt(6);
+
+  random() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Dicee"),
-      ),
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: Text('Go back!'),
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal.shade200,
+        appBar: AppBar(
+          title: Text("Dicee"),
+          backgroundColor: Colors.teal.shade300,
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: FlatButton(
+                    onPressed: () => random(),
+                    child: Image.asset('assets/images/dice$leftDiceNumber.png'),
+                  ),
+                ),
+                Expanded(
+                  child: FlatButton(
+                    onPressed: () => random(),
+                    child:
+                        Image.asset('assets/images/dice$rightDiceNumber.png'),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            RaisedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text("Go back"),
+            )
+          ],
         ),
       ),
     );
